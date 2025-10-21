aeb77ed77d7f247f6135a5d06b0320b1ed9353de
arifrahim <arifrahimjamal@gmail.com>
2025-10-21 21:46:04 +0300

    fixed the clubs misshap

--- app.py
+++ app.py
@@ -32,7 +32,9 @@ app = Flask(__name__)
 # UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
 # os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = '/home/arifrahim/Projects/Khamishigh/Khamis-high-Backend/uploads'
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
@@ -74,11 +76,12 @@ db.init_app(app)
 def home():
     return jsonify(message="Welcome to the Khamis High Backend API")
 
-# # ✅ Serve uploaded files
-# @app.route('/uploads/<path:filename>')
-# def serve_uploaded_file(filename):
-#     """Serve uploaded images and files safely."""
-#     return send_from_directory(UPLOAD_FOLDER, filename)
+# ✅ Serve uploaded files
+@app.route('/uploads/<path:filename>')
+def serve_uploaded_file(filename):
+    """Serve uploaded images and files safely."""
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
 
 # ✅ RESTful endpoints
 api.add_resource(SystemResource, '/systems', '/systems/<int:system_id>')
